94de2586b44b7a2b427932cbf9c92a57d42937f4
James Preston <jamescpreston93@gmail.com>
2019-04-21 20:08:06 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public KeyCode UpKey;
+    public KeyCode DownKey;
+
+    private float speed = 6.0f;
+
+    void Update()
+    {
+        var movement = new Vector3();
+        if (Input.GetKey(UpKey))
+        {
+            movement += Vector3.up;
+        }
+
+        if (Input.GetKey(DownKey))
+        {
+            movement += Vector3.down;
+        }
+
+        transform.position += movement * speed * Time.deltaTime;
+    }
+
+}
