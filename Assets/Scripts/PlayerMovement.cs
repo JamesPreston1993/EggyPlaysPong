66752a4554ebe580851b3369872267e4ce8c94e0
James Preston <jamescpreston93@gmail.com>
2019-04-21 23:08:34 +0100

    Add idle animation and set animation state

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,18 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(Animator))]
 public class PlayerMovement : MonoBehaviour
 {
     public KeyCode UpKey;
     public KeyCode DownKey;
 
     private float speed = 6.0f;
+
+    private Animator animator;
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
 
     void Update()
     {
@@ -20,7 +27,16 @@ public class PlayerMovement : MonoBehaviour
             movement += Vector3.down;
         }
 
+        SetAnimationState(movement.y != 0.0f);
+
         transform.position += movement * speed * Time.deltaTime;
     }
 
+    private void SetAnimationState(bool isWalking)
+    {
+        if (animator.GetBool("IsWalking") != isWalking)
+        {
+            animator.SetBool("IsWalking", isWalking);
+        }
+    }
 }
