bca724c2c8644b0b632a5af438eefb25626fa969
James Preston <jamescpreston93@gmail.com>
2019-04-21 20:37:19 +0100

    Spawn a ball and send it in a random direction left or right

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Random = System.Random;
+
+public class BallMovement : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D ballRigidbody;
+    private Random random;
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        random = new Random();
+        var direction = random.Next(1, 10) % 2 == 0 
+            ? Vector2.left
+            : Vector2.right;
+        ballRigidbody.AddForce(direction * speed);
+    }
+}
