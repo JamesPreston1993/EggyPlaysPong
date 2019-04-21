72db9f165599a125420ad1fb7e90e728bca2a5f2
James Preston <jamescpreston93@gmail.com>
2019-04-22 00:42:48 +0100

    Add main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	void Update ()
+    {
+	    if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Game");
+        }
+    }
+}
