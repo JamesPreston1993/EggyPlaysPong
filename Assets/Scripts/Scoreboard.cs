ffc6fa159c75a3c45d0b179bec351c82089def2e
James Preston <jamescpreston93@gmail.com>
2019-04-21 21:38:01 +0100

    Display score

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,14 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class Scoreboard : MonoBehaviour
+{
+    public TextMeshProUGUI playerOneScore;
+    public TextMeshProUGUI playerTwoScore;
+
+    void Update ()
+    {
+        playerOneScore.SetText(GameManager.Instance.PlayerOneScore.ToString());
+        playerTwoScore.SetText(GameManager.Instance.PlayerTwoScore.ToString());
+    }
+}
